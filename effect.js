f340c8facb3af836e68503e02100669a2128cac0
KD1209 <kewald1293@gmail.com>
2026-03-14 22:14:41 +0530

    Add files via upload

---
+++ effect.js
@@ -0,0 +1,103 @@
+$(window).load(function(){
+	$('.loading').fadeOut('fast');
+	$('.container').fadeIn('fast');
+});
+
+$('document').ready(function(){
+
+	/* ── Turn on lights ── */
+	$('#turn_on').click(function(){
+		$('#bulb_yellow').addClass('bulb-glow-yellow');
+		$('#bulb_red').addClass('bulb-glow-red');
+		$('#bulb_blue').addClass('bulb-glow-blue');
+		$('#bulb_green').addClass('bulb-glow-green');
+		$('#bulb_pink').addClass('bulb-glow-pink');
+		$('#bulb_orange').addClass('bulb-glow-orange');
+		$('body').addClass('peach');
+		$(this).fadeOut('slow').delay(5000).promise().done(function(){
+			$('#play').fadeIn('slow');
+		});
+	});
+
+	/* ── Play music ── */
+	$('#play').click(function(){
+		var audio = $('.song')[0];
+		audio.play();
+		$('#bulb_yellow').addClass('bulb-glow-yellow-after');
+		$('#bulb_red').addClass('bulb-glow-red-after');
+		$('#bulb_blue').addClass('bulb-glow-blue-after');
+		$('#bulb_green').addClass('bulb-glow-green-after');
+		$('#bulb_pink').addClass('bulb-glow-pink-after');
+		$('#bulb_orange').addClass('bulb-glow-orange-after');
+		$('body').addClass('peach-after');
+		$(this).fadeOut('slow').delay(6000).promise().done(function(){
+			$('#bannar_coming').fadeIn('slow');
+		});
+	});
+
+	/* ── Banner drop ── */
+	$('#bannar_coming').click(function(){
+		$('.bannar').addClass('bannar-come');
+		$(this).fadeOut('slow').delay(6000).promise().done(function(){
+			$('#balloons_flying').fadeIn('slow');
+		});
+	});
+
+	/* ── Balloon border fly off ── */
+	$('#balloons_flying').click(function(){
+		$('.balloon-border').animate({top: -500}, 8000);
+		$(this).fadeOut('slow').delay(5000).promise().done(function(){
+			$('#cake_fadein').fadeIn('slow');
+		});
+	});
+
+	/* ── Show cake ── */
+	$('#cake_fadein').click(function(){
+		$('.cake').fadeIn('slow');
+		$(this).fadeOut('slow').delay(3000).promise().done(function(){
+			$('#light_candle').fadeIn('slow');
+		});
+	});
+
+	/* ── Light candle ── */
+	$('#light_candle').click(function(){
+		$('.fuego').fadeIn('slow');
+		$(this).fadeOut('slow').promise().done(function(){
+			$('#wish_message').fadeIn('slow');
+		});
+	});
+
+	/* ── Happy Birthday – show name ── */
+	$('#wish_message').click(function(){
+		$('#name-display').fadeIn(1500);
+		$(this).fadeOut('slow').delay(3000).promise().done(function(){
+			$('#story').fadeIn('slow');
+		});
+	});
+
+	/* ── Story / message loop ── */
+	$('#story').click(function(){
+		$(this).fadeOut('slow');
+		$('#name-display').fadeOut('slow');
+		$('.cake').fadeOut('fast').promise().done(function(){
+			$('.message').fadeIn('slow');
+		});
+
+		function msgLoop(i) {
+			$("p:nth-child(" + i + ")").fadeOut('slow').delay(4000).promise().done(function(){
+				i = i + 1;
+				$("p:nth-child(" + i + ")").fadeIn('slow').delay(1000);
+				if(i == 50){
+					$("p:nth-child(49)").fadeOut('slow').promise().done(function(){
+						$('.cake').fadeIn('fast');
+					});
+				} else {
+					msgLoop(i);
+				}
+			});
+		}
+
+		msgLoop(0);
+	});
+
+});
